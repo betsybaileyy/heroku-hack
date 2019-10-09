ddec804300074d55c0bf1570fd5d01d781b25fa0
Ansel Bridgewater <iamansel@gmail.com>
2019-10-08 18:19:34 -0700

    Added doc string to deplete_hours()

--- hack.py
+++ hack.py
@@ -1,9 +1,21 @@
-"""A function to deplete a heroku free tier user's dyno hours."""
+"""This module depletes a heroku free tier user's dyno hours."""
 import time
 import urllib.request
 
 
 def deplete_hours(app_links):
+    """
+    Pings the same user's heroku apps to deplete their hours.
+
+    This function runs until the user manually stops it. It
+    makes requests to the apps every 30 minutes to keep the
+    apps awake.
+
+    Args:
+        app_links (arr): An array of strings which contain
+            the urls for heroku apps.
+
+    """
     while True:
         for link in app_links:
             urllib.request.urlopen(link)
