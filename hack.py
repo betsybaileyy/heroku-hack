2f4397ca9ea631c9628df50037aba2bf22a4688e
Ansel Bridgewater <iamansel@gmail.com>
2019-10-08 18:03:19 -0700

    Added doc string and refactored code

--- hack.py
+++ hack.py
@@ -1,18 +1,25 @@
+"""A function to deplete a heroku free tier user's dyno hours."""
 import time
 import urllib.request
 
-# List the heroku app links here
-heroku_apps = [
-    "https://app1.herokuapp.com/",
-    "https://app2.herokuapp.com/",
-    "https://app3.herokuapp.com/"
-]
 
-while True:
-    for app in heroku_apps:
-        urllib.request.urlopen(app)
-        print("Called: ", app)
+def deplete_hours(app_links):
+    while True:
+        for link in app_links:
+            urllib.request.urlopen(link)
+            print("Called: ", link)
 
-    print("Now sleeping for 30 minutes")
-    seconds_in_30_min = 30 * 60
-    time.sleep(seconds_in_30_min)
+        print("Now sleeping for 30 minutes")
+        seconds_in_30_min = 30 * 60
+        time.sleep(seconds_in_30_min)
+
+
+if __name__ == '__main__':
+    # List the heroku app links here
+    heroku_apps = [
+        "https://app1.herokuapp.com/",
+        "https://app2.herokuapp.com/",
+        "https://app3.herokuapp.com/"
+    ]
+
+    deplete_hours(heroku_apps)
