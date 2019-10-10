f0392aecf00f15834a46a5e701ec231b21f6db71
Ansel Bridgewater <iamansel@gmail.com>
2019-10-10 14:29:57 -0700

    Added comments

--- defense_off.sh
+++ defense_off.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# Change the app names to your Heroku app names
 heroku_projects=(app-1 app-2 app-3)
 
+# Turn the web dyno off for each listed project above
 for i in ${heroku_projects[@]}; do
   heroku ps:scale web=0 -a ${i}
 done
