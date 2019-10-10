e7c194a01d054658adbb8b4561ea61d00da14f9d
Ansel Bridgewater <iamansel@gmail.com>
2019-10-10 14:30:06 -0700

    Added comments

--- defense_on.sh
+++ defense_on.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# Change the app names to your Heroku app names
 heroku_projects=(app-1 app-2 app-3)
 
+# Turn the web dyno on for each listed project above
 for i in ${heroku_projects[@]}; do
   heroku ps:scale web=1 -a ${i}
 done
